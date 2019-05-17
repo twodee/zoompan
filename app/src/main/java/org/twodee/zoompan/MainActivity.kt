de25270b708dabb13afa7ba71b6d7675c6022f22
Chris Johnson <crjjrc@gmail.com>
2019-05-17 21:29:53 +1200

    Prime repository with first draft.

---
+++ app/src/main/java/org/twodee/zoompan/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.twodee.zoompan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    enableFullscreen()
+  }
+
+  private fun enableFullscreen() {
+    window.decorView.systemUiVisibility =
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+  }
+}
